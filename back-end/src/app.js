6f0b48b7b1b9312a4b4b14a039720cc4431a1196
George Redivo <georgeredivo@gmail.com>
2023-09-27 19:23:29 -0300

    Initial fix for back/front integration
    Remove 'no-cors' from front end and add CORS support in back end.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
 
 // Configure body parser for JSON requests
 app.use(bodyParser.json());
